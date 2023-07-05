79949efc25673f98d0645436baec8c184d6b4ce2
Nishu Singh <93818706+emailnishus@users.noreply.github.com>
2023-07-05 19:45:14 +0530

    Leaders in Array

---
+++ Leaders_in_array.java
@@ -0,0 +1,28 @@
+import java.io.*;
+/**
+ * Leaders_in_array
+ */
+class Leaders_in_array {
+    public void leaders(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            int j;
+            for(j = i + 1; j < size; j++){
+                if(arr[i] <= arr[j])
+                   break;
+            }
+            if(j == size)
+               System.out.print(arr[i]+" ");
+            
+        }
+        
+    }
+
+    public static void main(String[] args){
+        Leaders_in_array lead = new Leaders_in_array();
+        int arr[] = new int[]{16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+        lead.leaders(arr, n);
+    }
+
+    
+}
